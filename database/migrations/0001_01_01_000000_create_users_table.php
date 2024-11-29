a4036be6e8b67a0f6ec85fd71e3408351ad8dee7
Bhaudik <pumbhadiyabhaudik@gmail.com>
2024-11-29 17:58:12 +0530

    Home page setup done

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('designation')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
